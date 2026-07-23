091a4116cbe39bfd6b0b91d889462955ade26899
Wendell <dev.wendellsantos@gmail.com>
2026-07-23 19:55:12 -0300

    Organiza rotas usando Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+    from app.routes.main import main
+    app.register_blueprint(main)
 
     return app
